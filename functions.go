60326956e9018f6336fa9544a9afb842c3a3bf6a
Zain Fathoni <zain.fathoni@gmail.com>
2017-09-06 23:59:22 +0700

    add: functions.go

--- functions.go
+++ functions.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func sum(x, y, z int) int {
+	return x + y + z
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func waris(total int) (son, daughter int) {
+	son = total * 2 / 3
+	daughter = total * 1 / 3
+	return
+}
+
 func main() {
-	fmt.Println()
+	fmt.Println(sum(1, 2, 3))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+
+	fmt.Println(waris(9))
 }
